9e0f4652c95dfbae7371db2c260ad55c5546b2ab
EmrermE <77544437+EmrermE@users.noreply.github.com>
2025-08-10 19:42:50 +0300

    Update ceaser.py

--- ceaser.py
+++ ceaser.py
@@ -1,28 +1,29 @@
+alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
+letters = list(alphabet)
+thisdict = {letters[i]: i for i in range(len(letters))}
+dictthis = {i: letters[i] for i in range(len(letters))}
+n = len(alphabet)
+
 def encrypt(message, key):
-    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz " #You can change the alphabet as you want
-    letters = list(alphabet)
-    thisdict = {letters[i]: i for i in range(len(letters))}
-    dictthis = {i: letters[i] for i in range(len(letters))}
     encryption = ""
-    for i in message:
-        encryption += dictthis[((thisdict[i] + key) % len(thisdict))]
+    for ch in message:
+        if ch not in thisdict:
+            raise ValueError(f"Invalid character: {ch}")
+        encryption += dictthis[(thisdict[ch] + key) % n]
     return encryption
+
 def decrypt(message, key):
-    alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz "
-    letters = list(alphabet)
-    thisdict = {letters[i]: i for i in range(len(letters))}
-    dictthis = {i: letters[i] for i in range(len(letters))}
     decryption = ""
-    for i in message:
-        decryption += dictthis[((thisdict[i] - key) % len(thisdict))]
+    for ch in message:
+        if ch not in thisdict:
+            raise ValueError(f"Invalid character: {ch}")
+        decryption += dictthis[(thisdict[ch] - key) % n]
     return decryption
 
-
-
-
-
-
-
-
-
-
+# Example
+msg = ""
+key = 7
+enc = encrypt(msg, key)
+dec = decrypt(enc, key)
+print("Encrypted:", enc)
+print("Decrypted:", dec)
